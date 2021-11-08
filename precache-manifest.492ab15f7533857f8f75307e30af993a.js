01e12957291e7bf9aa9e171e8c36e000acf19725
Kirill <3331668@mail.ru>
2021-11-08 16:11:05 +0300

    Updates

---
+++ precache-manifest.492ab15f7533857f8f75307e30af993a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4198dabbe2e819dc043d8aeefc69158e",
+    "url": "/EnglishWordLearn/index.html"
+  },
+  {
+    "revision": "edb23dda51b6431857e7",
+    "url": "/EnglishWordLearn/static/css/main.91a8c386.chunk.css"
+  },
+  {
+    "revision": "42b16a6e12229a40aeb5",
+    "url": "/EnglishWordLearn/static/js/2.9129ab34.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/EnglishWordLearn/static/js/2.9129ab34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb23dda51b6431857e7",
+    "url": "/EnglishWordLearn/static/js/main.cc7ea1d0.chunk.js"
+  },
+  {
+    "revision": "d0dd7f022dd981895193",
+    "url": "/EnglishWordLearn/static/js/runtime-main.caee6869.js"
+  }
+]);
