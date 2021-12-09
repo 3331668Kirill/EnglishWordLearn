aabe80ae6464e8d9622c902661334a3ae65ebe37
Kirill <3331668@mail.ru>
2021-12-09 10:50:42 +0300

    Updates

--- precache-manifest.249d23c302ae674759f4f2a37858af27.js
+++ precache-manifest.249d23c302ae674759f4f2a37858af27.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d42ce1d7f5b26728df83650ab574f63a",
+    "revision": "92151a04f3943df7fce711c4949fdc9e",
     "url": "/EnglishWordLearn/index.html"
   },
   {
-    "revision": "859b5f71b0c92ba7549b",
+    "revision": "0bbda8add5425239c764",
     "url": "/EnglishWordLearn/static/css/main.60d2df74.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/EnglishWordLearn/static/js/2.9129ab34.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "859b5f71b0c92ba7549b",
-    "url": "/EnglishWordLearn/static/js/main.764806af.chunk.js"
+    "revision": "0bbda8add5425239c764",
+    "url": "/EnglishWordLearn/static/js/main.ad92afeb.chunk.js"
   },
   {
     "revision": "d0dd7f022dd981895193",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/EnglishWordLearn/precache-manifest.6259a7796c7b6389c8e56f8068cb7aa5.js"
+  "/EnglishWordLearn/precache-manifest.249d23c302ae674759f4f2a37858af27.js"
 );
 
 self.addEventListener('message', (event) => {
